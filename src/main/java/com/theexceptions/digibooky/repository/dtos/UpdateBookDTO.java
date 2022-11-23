5f4020f327b7045aedac3a898584fb163e9a641d
MarriVdr <marriaan@gmail.com>
2022-11-23 16:39:27 +0100

    02 Show details of book

---
+++ src/main/java/com/theexceptions/digibooky/repository/dtos/UpdateBookDTO.java
@@ -0,0 +1,32 @@
+package com.theexceptions.digibooky.repository.dtos;
+
+public class UpdateBookDTO {
+    private String title;
+    private String smallSummary;
+    private String authorFirstName;
+    private String authorLastName;
+
+
+    public UpdateBookDTO(String title, String smallSummary, String authorFirstName, String authorLastName) {
+        this.title = title;
+        this.smallSummary = smallSummary;
+        this.authorFirstName = authorFirstName;
+        this.authorLastName = authorLastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSmallSummary() {
+        return smallSummary;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+}
